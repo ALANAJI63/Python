c39ba2e75e9a9eefe52407d000ea8ac717f979d1
Alan A <alanaji63@gmail.com>
2025-08-08 21:05:54 +0530

    python array and dictionary

---
+++ more_data_types.py
@@ -0,0 +1,22 @@
+# Array
+# arr = []
+# arr.append(123)
+# arr.append(456)
+# arr.append("alan")
+# print(arr)
+
+# Dictionary
+
+marks = {}
+print(type(marks))
+
+marks["Maths"] = 94
+marks["Science"] = 92
+marks["SS"] = 80.5
+marks[123] = 100
+marks[[1,2,3]] = 122
+
+print(marks)
+print(f"type of math marks is {marks["Maths"]}")
+print(f"type of SS marks is {marks["SS"]}")
+
