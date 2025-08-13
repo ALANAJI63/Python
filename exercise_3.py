1342369c2146e57845c24e31c921c0a70f1ee239
Alan A <alanaji63@gmail.com>
2025-08-13 20:06:22 +0530

    Adult_or_Minor

---
+++ exercise_3.py
@@ -0,0 +1,7 @@
+year = int(input("Enter your birth year"))
+age = 2025 - year
+if age>18:
+    print(f"Your are is {age} years old")
+    print("You are an Adult")
+else:
+    print("You are a Minor")
