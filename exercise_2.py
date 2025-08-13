06200719a1184c614ab56ed2e3f1eee0af7424a0
Alan A <alanaji63@gmail.com>
2025-08-13 19:59:39 +0530

    Add_two_files_updated

--- exercise_2.py
+++ exercise_2.py
@@ -1,7 +1,4 @@
 def sum_of_two(a,b):
-    return a+b
-a = int(input("Enter the first number"))
-b = int(input("Enter the second number"))
-print(f"Sum of {a} and {b} is {sum_of_two(a,b)}")
+    print(f"Sum of {a} and {b} is {a+b}")
 
 sum_of_two(4,5)
